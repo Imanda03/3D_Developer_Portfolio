3540830b8faa95af6cc555b68c1c33ef0086909d
Your Name <asis03ktm@gmail.com>
2026-01-09 22:37:33 +0545

    update experience

--- src/assets/index.js
+++ src/assets/index.js
@@ -25,6 +25,7 @@ import meta from "./company/meta.png";
 import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
+import kidcode from "./company/kidncode.png";
 
 import carrent from "./carrent.png";
 import jobit from "./jobit.png";
@@ -59,6 +60,7 @@ export {
   shopify,
   starbucks,
   tesla,
+  kidcode,
   carrent,
   jobit,
   tripguide,
